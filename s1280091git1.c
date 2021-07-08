0b77db98242ab41b4ac7371de74c8228bf45e484
s1280091 <87132020+s1280091@users.noreply.github.com>
2021-07-08 22:55:53 +0900

    Initial revision

---
+++ s1280091git1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h> 
+
+int main(){
+    srand(time(NULL));
+    int T,d1=rand() % 7,d2=rand() % 7;
+    T=d1+d2;
+    
+    printf("Rolling the dice...\nDie 1:%d\n",d1);
+    printf("Die 2:%d\n",d2);
+
+    printf("Total value: %d",T);
+
+  return 0;
+}
